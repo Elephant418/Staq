a5472292c892bef2b004bba22972838e8976c900
Thomas ZILLIOX <thomas@zilliox.me>
2012-06-21 11:28:48 +0200

    Add a temporary default controller

--- Application.php
+++ Application.php
@@ -18,6 +18,8 @@ class Application {
 	public static $root_paths = array( );
 	private $root_path;
 	private $rootes = array( );
+	// TODO: Supprimer le default controller !
+	public $default_controller = 'welcome';
 
 
 	/*************************************************************************
@@ -56,6 +58,77 @@ class Application {
 			self::$root_paths[ $module_name ] = $this->root_path . $module_path . '/';
 		}
 	}
+	public function render( ) {
+		try {
+			return $this->route( );
+		} catch( \Exception\Redirect $e ) {
+			\Message::push( $e->getMessage( ), \Message::EXCEPTION );
+			$this->change_route( $e->route );
+			return $this->render( );
+		/*} catch( \Exception $e ) {
+			\Message::push( $e->getMessage( ), \Message::EXCEPTION );
+			$this->change_route( '/error/view/500' );
+			return $this->render( );*/
+		}
+	}
 	public function run( ) {
+		echo $this->render( );
+	}
+
+
+	/*************************************************************************
+	  STATIC METHODS                   
+	 *************************************************************************/
+	public static function redirect( $route ) {
+		header( 'HTTP/1.1 302 Moved Temporarily' );
+		header( 'Location: ' . $route );
+		die();
+	}
+
+
+	/*************************************************************************
+	  PRIVATE METHODS                   
+	 *************************************************************************/
+	private function current_route( ) {
+		return end( $this->routes );
+	}
+	private function change_route( $route ) {
+		if ( in_array( $route, $this->routes ) ) {
+			throw new \Exception( 'Redirecting loop detected' );
+		}
+		$this->routes[ ] = $route;
+	}
+	private function route( ) {
+		$url_parts = explode( '/', substr( $this->current_route( ), 1 ) );
+
+		// Controller
+		if ( strlen( $this->current_route( ) ) == 1 ) {
+			$controller_name = $this->default_controller;
+		} else {
+			$controller_name = $url_parts[ 0 ];
+		}
+		try {		
+			$controller_name = '\\Controller\\' . ucfirst( $controller_name );
+			$this->controller = new $controller_name( );
+		} catch ( Exception $e ) {
+			var_dump( $e );
+			// throw new \Exception\Redirect( '/error/view/404', 'Unknown controller "' . $controller_name . '"' );
+		}
+
+		// Action
+		if ( count( $url_parts ) < 2 ) {
+			$this->action = $this->controller->action_method( );
+		} else {
+			$this->action = $this->controller->action_method( $url_parts[ 1 ] );
+		}
+		if ( ! is_callable( $this->callable_action( ) ) ) {
+			throw new \Exception\Redirect( '/error/view/404', 'Unknown controller\'s action "' . $controller_name . '::' . $this->action .'"' );
+		}
+
+		$this->parameters = array_slice( $url_parts, 2 );
+		return call_user_func_array( $this->callable_action( ), $this->parameters );
+	}
+	private function callable_action( ) {
+		return array( $this->controller, $this->action );
 	}
 }
