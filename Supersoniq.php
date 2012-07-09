4f8286ff35c486d68e62aa2e036df504dbfb5148
Thomas ZILLIOX <thomas@zilliox.me>
2012-07-09 17:59:45 +0200

    Rename var & methods form pattern to listening

--- Supersoniq.php
+++ Supersoniq.php
@@ -34,13 +34,13 @@ class Supersoniq {
 	/*************************************************************************
 	  CONFIGURATION METHODS                   
 	 *************************************************************************/
-	public function application( $application, $pattern = '/' ) {
+	public function application( $application, $listenings = '/' ) {
 		$application_name = $this->uniform_module_name( $application );
-		$this->applications[ $application_name ] = $pattern;
+		$this->applications[ $application_name ] = $listenings;
 		return $this;
 	}
-	public function platform( $platform, $pattern = '/' ) {
-		$this->platforms[ $platform ] = $pattern;
+	public function platform( $platform, $listenings = '/' ) {
+		$this->platforms[ $platform ] = $listenings;
 		return $this;
 	}
 
@@ -69,60 +69,60 @@ class Supersoniq {
 
 
 	/*************************************************************************
-	  URL PATTERN                  
+	  APPLICATION & PLATFORM LISTENING                  
 	 *************************************************************************/
 	private function select_platform( ) {
-		foreach ( $this->platforms as $platform => $pattern ) {
-			if ( $host = $this->is_pattern_match( $pattern ) ) {
+		foreach ( $this->platforms as $platform => $listenings ) {
+			if ( $host = $this->handle_request( $listenings ) ) {
 				return $platform;
 			}
 		}
 	}
 	private function select_application( ) {
-		foreach ( $this->applications as $application => $pattern ) {
-			if ( $this->is_pattern_match( $pattern ) ) {
+		foreach ( $this->applications as $application => $listenings ) {
+			if ( $this->handle_request( $listenings ) ) {
 				return $application;
 			}
 		}
 	}
-	private function is_pattern_match( $patterns ) {
-		if ( ! is_array( $patterns ) ) {
-			$patterns = array( $patterns );
+	private function handle_request( $listenings ) {
+		if ( ! is_array( $listenings ) ) {
+			$listenings = array( $listenings );
 		}
-		foreach ( $patterns as $pattern ) {
-			$method_name = 'is_pattern_match_by_uri';
-			if ( \Supersoniq\starts_with( $pattern, array( 'http://', 'https://', '//' ) ) ) {
-				$method_name = 'is_pattern_match_by_url';
-			} else if ( \Supersoniq\starts_with( $pattern, ':' ) ) {
-				$method_name = 'is_pattern_match_by_port';
+		foreach ( $listenings as $listening ) {
+			$method_name = 'handle_request_by_uri';
+			if ( \Supersoniq\starts_with( $listening, array( 'http://', 'https://', '//' ) ) ) {
+				$method_name = 'handle_request_by_url';
+			} else if ( \Supersoniq\starts_with( $listening, ':' ) ) {
+				$method_name = 'handle_request_by_port';
 			}
-			if ( $this->$method_name( $pattern ) ) {
+			if ( $this->$method_name( $listening ) ) {
 				return TRUE;
 			}
 		}
 		return FALSE;
 	}
-	private function is_pattern_match_by_url( $pattern ) {
+	private function handle_request_by_url( $listen_url ) {
 		$base_request = $this->request_base_url( );
 		$full_request = $base_request . $this->request_uri;
-		$pattern      = \Supersoniq\substr_after( $pattern, '//' );
-		$pattern      = \Supersoniq\must_not_ends_with( $pattern, '/' );
-		// echo $base_request . ' &lt; ' . $pattern . ' &lt; ' . $full_request . HTML_EOL;
-		if ( \Supersoniq\starts_with( $pattern, $base_request ) && \Supersoniq\starts_with( $full_request, $pattern ) ) {
-			$this->request_base_uri .= \Supersoniq\substr_after( $pattern, $base_request );
-			$this->request_uri = \Supersoniq\substr_after( $full_request, $pattern );
+		$listen_url   = \Supersoniq\substr_after( $listen_url, '//' );
+		$listen_url   = \Supersoniq\must_not_ends_with( $listen_url, '/' );
+		// echo $base_request . ' &lt; ' . $listen_url . ' &lt; ' . $full_request . HTML_EOL;
+		if ( \Supersoniq\starts_with( $listen_url, $base_request ) && \Supersoniq\starts_with( $full_request, $listen_url ) ) {
+			$this->request_base_uri .= \Supersoniq\substr_after( $listen_url, $base_request );
+			$this->request_uri = \Supersoniq\substr_after( $full_request, $listen_url );
 			return TRUE;
 		}
 		return FALSE;
 	}
-	private function is_pattern_match_by_uri( $base_uri ) {
-		return $this->is_pattern_match_by_url( '//' . $this->request_base_url( $base_uri ) );
+	private function handle_request_by_uri( $listen_uri ) {
+		return $this->handle_request_by_url( '//' . $this->request_base_url( $listen_uri ) );
 	}
-	private function is_pattern_match_by_port( $pattern ) {
-		$pattern  = \Supersoniq\substr_after( $pattern, ':' );
-		$port     = \Supersoniq\substr_before( $pattern, '/' );
-		$base_uri = \Supersoniq\substr_after( $pattern, '/' );
-		return $this->is_pattern_match_by_url( '//' . $this->request_base_url( '/' . $base_uri, $port ) );
+	private function handle_request_by_port( $listen_port_uri ) {
+		$listen_port_uri =       \Supersoniq\substr_after( $listen_port_uri, ':' );
+		$listen_port     =      \Supersoniq\substr_before( $listen_port_uri, '/' );
+		$listen_base_uri = '/' . \Supersoniq\substr_after( $listen_port_uri, '/' );
+		return $this->handle_request_by_url( '//' . $this->request_base_url( $listen_base_uri, $listen_port ) );
 	}
 	private function request_base_url( $base_uri = NULL, $port = NULL ) {
 		$request_base_url = $_SERVER[ 'SERVER_NAME' ];
