0871bb459c04f18f21546ced9ba680042261f004
Thomas ZILLIOX <thomas@zilliox.me>
2012-07-17 02:58:29 +0200

    Display errors

--- Kernel/test/index.php
+++ Kernel/test/index.php
@@ -5,6 +5,9 @@
  * For more information, please refer to <http://unlicense.org/>
  */
 
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+
 include_once( '../../Supersoniq/include.php' );
 
 require_once( dirname( __FILE__ ) . '/Supersoniq.php' );
