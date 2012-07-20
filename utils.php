11af4197c1f8f746ac213e3a50099a720c69fe33
Thomas ZILLIOX <thomas@zilliox.me>
2012-07-20 02:50:02 +0200

    Add module route & url methods

--- utils.php
+++ utils.php
@@ -12,6 +12,7 @@ namespace Supersoniq;
   STRING METHODS                   
  *************************************************************************/
 
+
 // STARTS WITH & ENDS WITH FUNCTIONS
 function starts_with( $hay, $needles ) {
 	must_be_array( $needles );
@@ -22,6 +23,7 @@ function starts_with( $hay, $needles ) {
 	}
 	return FALSE;
 }
+
 function ends_with( $hay, $needles ) {
 	must_be_array( $needles );
 	foreach( $needles as $needle ) {
@@ -31,40 +33,53 @@ function ends_with( $hay, $needles ) {
 	}
 	return FALSE;
 }
+
 function i_starts_with( $hay, $needle ) {
 	return starts_with( strtolower( $hay ), strtolower( $needle ) );
 }
+
 function i_ends_with( $hay, $needle ) {
 	return ends_with( strtolower( $hay ), strtolower( $needle ) );
 }
+
 function must_starts_with( &$hay, $needle ) {
 	if ( ! starts_with( $hay, $needle ) ) {
 		$hay = $needle . $hay;
 	}
 }
+
 function must_ends_with( &$hay, $needle ) {
 	if ( ! ends_with( $hay, $needle ) ) {
 		$hay .= $needle;
 	}
 }
+
 function must_not_starts_with( &$hay, $needle ) {
 	if ( starts_with( $hay, $needle ) ) {
 		$hay = substr( $hay, strlen( $needle ) );
 	}
 }
+
 function must_not_ends_with( &$hay, $needle ) {
 	if ( ends_with( $hay, $needle ) ) {
 		$hay = substr( $hay, 0, -strlen( $needle ) );
 	}
 }
 
+
+
 // CONTAINS FUNCTIONS
 function contains( $hay, $needle ) {
-	return ( strpos( $hay, $needle ) !== false );
+	// if ( ! empty( $needle ) ) {
+		return ( strpos( $hay, $needle ) !== false );
+	// }
 }
+
 function i_contains( $hay, $needle ) {
 	return contains( strtolower( $hay ), strtolower( $needle ) );
 }
+
+
 
 // SUBSTRING FUNCTIONS
 function cut_before( &$hay, $needles ) {
@@ -72,11 +87,12 @@ function cut_before( &$hay, $needles ) {
 	$hay = substr( $hay, strlen( $return ) );
 	return $return;
 }
+
 function substr_before( $hay, $needles ) {
 	must_be_array( $needles );
 	$return = $hay;
 	foreach( $needles as $needle ) {
-		if ( contains( $hay, $needle ) ) {
+		if ( ! empty( $needle) && contains( $hay, $needle ) ) {
 			$cut = substr( $hay, 0, strpos( $hay, $needle ) );
 			if ( strlen( $cut ) < strlen ( $return ) ) {
 				$return = $cut;
@@ -86,16 +102,18 @@ function substr_before( $hay, $needles ) {
 	$hay = substr( $hay, strlen( $return ) );
 	return $return;
 }
+
 function cut_before_last( &$hay, $needles ) {
 	$return = substr_before_last( $hay, $needles );
 	$hay = substr( $hay, strlen( $return ) );
 	return $return;
 }
+
 function substr_before_last( $hay, $needles ) {
 	must_be_array( $needles );
 	$return = '';
 	foreach( $needles as $needle ) {
-		if ( contains( $hay, $needle ) ) {
+		if ( ! empty( $needle) && contains( $hay, $needle ) ) {
 			$cut = substr( $hay, 0, strrpos( $hay, $needle ) );
 			if ( strlen( $cut ) > strlen ( $return ) ) {
 				$return = $cut;
@@ -105,16 +123,18 @@ function substr_before_last( $hay, $needles ) {
 	$hay = substr( $hay, strlen( $return ) );
 	return $return;
 }
+
 function cut_after( &$hay, $needles ) {
 	$return = substr_after( $hay, $needles );
 	$hay = substr( $hay, 0, - strlen( $return ) );
 	return $return;
 }
+
 function substr_after( $hay, $needles ) {
 	must_be_array( $needles );
 	$return = '';
 	foreach( $needles as $needle ) {
-		if ( contains( $hay, $needle ) ) {
+		if ( ! empty( $needle) && contains( $hay, $needle ) ) {
 			$cut = substr( $hay, strpos( $hay, $needle ) + strlen( $needle ) );
 			if ( strlen( $cut ) > strlen ( $return ) ) {
 				$return = $cut;
@@ -123,16 +143,18 @@ function substr_after( $hay, $needles ) {
 	}
 	return $return;
 }
+
 function cut_after_last( &$hay, $needles ) {
 	$return = substr_after_last( $hay, $needles );
 	$hay = substr( $hay, 0, - strlen( $return ) );
 	return $return;
 }
+
 function substr_after_last( $hay, $needles ) {
 	must_be_array( $needles );
 	$return = $hay;
 	foreach( $needles as $needle ) {
-		if ( contains( $hay, $needle ) ) {
+		if ( ! empty( $needle) && contains( $hay, $needle ) ) {
 			$cut = substr( $hay, strrpos( $hay, $needle ) + strlen( $needle ) );
 			if ( strlen( $cut ) < strlen ( $return ) ) {
 				$return = $cut;
@@ -141,6 +163,8 @@ function substr_after_last( $hay, $needles ) {
 	}
 	return $return;
 }
+
+
 
 // RANDOM FUNCTIONS
 function random( $length = 10 ) {
@@ -161,6 +185,7 @@ function dirname( $path, $level = 1 ) {
 }
 
 
+
 /*************************************************************************
   ARRAY METHODS                   
  *************************************************************************/
@@ -169,9 +194,12 @@ function must_be_array( &$array ) {
 		$array = [ $array ];
 	}
 }
+
 function array_merge_unique( $array1, $array2 ) {
 	return array_values( array_unique( array_merge( $array1, $array2 ) ) );
 }
+
+
 
 /*************************************************************************
   SUPERSONIQ METHODS                   
@@ -179,8 +207,34 @@ function array_merge_unique( $array1, $array2 ) {
 function format_to_namespace( $path ) {
 	return str_replace( '/', '\\', $path );
 }
+
 function format_to_path( $namespace ) {
 	return str_replace( '\\', '/', $namespace );
+}
+
+
+
+/*************************************************************************
+  ACTION METHODS                   
+ *************************************************************************/
+function redirect( $url ) {
+	header( 'HTTP/1.1 302 Moved Temporarily' );
+	header( 'Location: ' . $url );
+	die( );
+}
+
+function redirect_to_module_side( $module, $side, $parameters = [ ] ) {
+	redirect( module_side_url( $module, $action, $parameters ) );
+}
+
+function module_side_url( $module, $side, $parameters = [ ] ) {
+	return \Supersoniq::$BASE_URL . $this->module_side_route( $module, $side, $parameters );
+}
+
+function module_side_route( $module, $side, $parameters = [ ] ) {
+	$module = \Supersoniq::$MODULES[ $module ];
+	$route = $module->get_side_route( $side, $parameters );
+	return $route;
 }
 
 
