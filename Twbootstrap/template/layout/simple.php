ff187a029f0fbff263f40d75f0105976990e5c98
Thomas ZILLIOX <thomas@zilliox.me>
2012-07-27 04:48:54 +0200

    Better with a back link n the logo

--- Twbootstrap/template/layout/simple.php
+++ Twbootstrap/template/layout/simple.php
@@ -22,10 +22,10 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</a>
-				<a class="brand" href="#"><?= $this->application_name ?></a>
+				<a class="brand" href="/"><?= $this->application_name ?></a>
 				<div class="nav-collapse">
 					<ul class="nav">
-						<li class="active"><a href="#">Home</a></li>
+						<li class="active"><a href="/">Home</a></li>
 						<li><a href="#about">About</a></li>
 						<li><a href="#contact">Contact</a></li>
 					</ul>
