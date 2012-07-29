4ec93266cd8aa7f8fe891688443c668db97f7012
Thomas ZILLIOX <thomas@zilliox.me>
2012-07-29 12:24:02 +0200

    Add delete model page

---
+++ Packadata/Kernel/view/module/model/Delete.php
@@ -0,0 +1,23 @@
+<?php
+
+/* This file is part of the Supersoniq project.
+ * Supersoniq is a free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org/>
+ */
+
+namespace Supersoniq\Packadata\Kernel\View\Module\Model;
+
+abstract class Delete extends Delete\__Parent {
+
+
+	/*************************************************************************
+	  RENDER METHODS                   
+	 *************************************************************************/
+        public function render( $parameters = [ ] ) {
+		$controller = $this->get_controller( );
+		$model = $controller->get( $parameters[ 'id' ] );
+		$controller->delete( $model );
+		\Supersoniq\redirect_to_page( 'all' );
+	}
+
+}
