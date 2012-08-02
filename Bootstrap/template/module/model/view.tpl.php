d2dc20a30bf4b0d83134ac89687d2356a94e74b5
Thomas ZILLIOX <thomas@zilliox.me>
2012-08-02 04:26:25 +0200

    remove create button from view

--- Bootstrap/template/module/model/view.tpl.php
+++ Bootstrap/template/module/model/view.tpl.php
@@ -3,7 +3,6 @@
 <?= $this->display( $this->model ); ?>
 
 <a class="btn" href="<?= $page_url( 'all' ) ?>"><i class="icon-th-list"></i> List</a>
-<a class="btn" href="<?= $page_url( 'create' ) ?>"><i class="icon-plus-sign"></i> Create</a>
 <a class="btn" href="<?= $page_url( 'edit', $this->model->id ) ?>"><i class="icon-pencil"></i> Edit</a>
 <?php if ( $this->model->is_versioned ) { ?>
 	<a class="btn" href="<?= $page_url( 'archive', $this->model->id ) ?>"><i class="icon-th-list"></i> Archives</a>
