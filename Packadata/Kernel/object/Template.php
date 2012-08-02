de3e386020a4eda8b285c38b9b7c5c529823551d
Thomas ZILLIOX <thomas@zilliox.me>
2012-08-02 04:24:26 +0200

    Change section comment, it is not a constructor but an accessor

--- Packadata/Kernel/object/Template.php
+++ Packadata/Kernel/object/Template.php
@@ -12,12 +12,13 @@ class Template extends Template\__Parent {
 
 
 	/*************************************************************************
-	  CONSTRUCTOR                 
+	  ACCESSOR                 
 	 *************************************************************************/
 	public function by_model( $model, $mode ) {
 		return $this->by_name( 'Model\\' . ucfirst( $mode ) . '\\' . $model->type )
 			->set( 'content', $model );
 	}
+
 	public function by_data_type( $data_type, $mode ) {
 		return $this->by_name( 'Data_Type\\' . ucfirst( $mode ) . '\\' . $data_type->type )
 			->set( 'content', $data_type );
