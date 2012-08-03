8422a2b1c1fb7d1a14a970b25cd60ed29f90f60d
AmauryVallier <amaury.vallier@gmail.com>
2012-08-03 11:56:49 +0200

    First controller for the archives: the main view

---
+++ Packadata/Kernel/view/module/model/Archives.php
@@ -0,0 +1,21 @@
+<?php
+
+/* This file is part of the Supersoniq project.
+ * Supersoniq is a free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org/>
+ */
+
+namespace Supersoniq\Packadata\Kernel\View\Module\Model;
+
+abstract class Archives extends Archives\__Parent {
+
+
+	/*************************************************************************
+	  RENDER METHODS                   
+	 *************************************************************************/
+	public function fill( $template, $parameters = [ ] ) {
+		$template->archives = $this->get_controller( )->archives( );
+		return $template;
+	}
+
+}
