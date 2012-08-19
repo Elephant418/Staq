74eee28d302a6e2c54cf268f810abd5bab511ca0
Thomas ZILLIOX <thomas@zilliox.me>
2012-08-20 01:46:45 +0200

    Add desired action to the get attribute

--- Bootstrap/template/model/edit.tpl.php
+++ Bootstrap/template/model/edit.tpl.php
@@ -1,5 +1,5 @@
 <?php
-foreach ( $this->content->get_attribute_fields( ) as $name ) {
+foreach ( $this->content->get_attribute_fields( 'set' ) as $name ) {
 	$attribute = $this->content->attribute( $name );
 ?>
 	<label><b><?= $name ?></b> : <?= $this->display( $attribute, 'edit' ) ?></label>
--- Bootstrap/template/model/view.tpl.php
+++ Bootstrap/template/model/view.tpl.php
@@ -1,5 +1,5 @@
 <?php
-foreach ( $this->content->get_attribute_fields( ) as $name ) {
+foreach ( $this->content->get_attribute_fields( 'get' ) as $name ) {
 	$attribute = $this->content->attribute( $name );
 ?>
 	<p><b><?= $name ?></b> : <?= $this->display( $attribute ) ?></p>
