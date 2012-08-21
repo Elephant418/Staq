d1ac1ffecc3d607e5c8b5c2752aa82e1098da471
Thomas ZILLIOX <thomas@zilliox.me>
2012-08-21 15:27:47 +0200

    Try to improve cache with Pragma

--- Kernel/module/Public_File.php
+++ Kernel/module/Public_File.php
@@ -57,9 +57,11 @@ class Public_File {
 		$content_type = $this->get_content_type( $file_path );
 		$resource     = fopen( $file_path, 'rb' );
 		$cache_time   = $this->get_public_file_cache_time( );
+		$pragma       = ( $cache_time > 0 ) ? 'public' : 'no-cache'; 
+		header( 'Pragma: ' . $pragma );
 		header( 'Content-type: ' . $content_type . '; charset: UTF-8' );
 		header( 'Content-length: ' . filesize( $file_path ) );
-		header( 'Cache-control: private, max-age=' . ( $cache_time - time( ) ) . ', pre-check=' . ( $cache_time - time( ) ), true );
+		header( 'Cache-control: max-age=' . ( $cache_time - time( ) ) . ', pre-check=' . ( $cache_time - time( ) ), true );
 		header( 'Expires: ' . gmdate( 'D, d M Y H:i:s \G\M\T', $cache_time ), true );
 		fpassthru( $resource );
 	}
