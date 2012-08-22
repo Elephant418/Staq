2191bc070ccf41e2174b52b5326d13fbb3117c1c
Thomas ZILLIOX <thomas@zilliox.me>
2012-08-22 05:34:20 +0200

    Uniformize date format before saving

--- Packadata/Kernel/data_type/Date.php
+++ Packadata/Kernel/data_type/Date.php
@@ -19,22 +19,31 @@ class Date extends \Data_Type\__Base {
 	 * 1/1/00 through 31/12/99 and 01/01/1900 through 31/12/2099
 	 * Matches invalid dates such as February 31st
 	 */
-	private $regex = "*\b(0?[1-9]|[12][0-9]|3[01])[- /.](0?[1-9]|1[012])[- /.](19|20)?[0-9]{2}\b*";
-	protected $format;
+	private $regex = "*\b(0?[1-9]|[12][0-9]|3[01])[- /.](0?[1-9]|1[012])[- /.]((19|20)?[0-9]{2})\b*";
 	
 
 	/*************************************************************************
 	 USER GETTER & SETTER
 	*************************************************************************/
 	public function set( $value ) {
-		if ( preg_match( $this->regex, $value ) || $value == '' ) {
+		$matches = [ ];
+		if ( preg_match( $this->regex, $value, $matches ) || $value == '' ) {
+			if ( $matches ) {
+				if ( strlen( $matches[ 1 ] ) == 1 ) {
+					$matches[ 1 ] = '0' . $matches[ 1 ];
+				}
+				if ( strlen( $matches[ 2 ] ) == 1 ) {
+					$matches[ 2 ] = '0' . $matches[ 2 ];
+				}
+				if ( strlen( $matches[ 3 ] ) == 2 ) {
+					$matches[ 3 ] = '20' . $matches[ 3 ];
+				}
+				$value = $matches[ 1 ] . '-' . $matches[ 2 ] . '-' . $matches[ 3 ];
+			}
 			$this->init( $value );
 		} else {
-			\Notification::push( 'Wrong Input for the date ! Hasn\'t been saved.', \Notification::NOTICE );
+			\Notification::push( 'Wrong Input for the date ! Hasn\'t been saved. Must be DD-MM-YYYY.', \Notification::ERROR );
 		}
-	}
-	public function get_format( ) {
-		return $this->format;
 	}
 
 
@@ -46,29 +55,14 @@ class Date extends \Data_Type\__Base {
 	 * @param the wanted type of format
 	 * @return the value on the wanted format
 	 */
-	public function format( $type = NULL ) {
-		if ( $type != NULL ) {
-			if ( $type == 'Year' )	{
-				$this->format = $this->format_year( );
-			} else if ( $type == 'Month' )	{
-				$this->format = $this->format_month( );
-			} else if ( $type == 'Day' ) {
-				$this->format = $this->format_day( ); }
-			else { 
-				$this->format = date_format( \DateTime::createFromFormat( 'd/m/Y', $this->value ), $type );
-			}
-		} else {
-			return $this->format = $this->format_day( );
+	public function format( $type = NULL, $value = NULL ) {
+		if ( is_null( $value ) ) {
+			$value = $this->value;
 		}
-		return $this->format;
-	}
-	private function format_year( ) {
-		return date_format( \DateTime::createFromFormat( 'd/m/Y', $this->value ), 'Y' );
-	}
-	private function format_month( ) {
-		return date_format( \DateTime::createFromFormat( 'd/m/Y', $this->value ), 'Y/m' );
-	}
-	private function format_day( ) {
-		return date_format( \DateTime::createFromFormat( 'd/m/Y', $this->value ), 'Y/m/d' );
+		else if ( $type == 'Year' ) $formater = 'Y';
+		else if ( $type == 'Month' ) $formater = 'Y-m';
+		else if ( $type == 'Day' || is_null( $type ) ) $formater = 'Y-m-d';
+		else $formater = $type;
+		return date_format( \DateTime::createFromFormat( 'd/m/Y', $value ), $formater );
 	}
 }
