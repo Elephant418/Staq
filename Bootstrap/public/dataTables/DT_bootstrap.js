3cab2632dc97077f5f9cac5032ee4fbc31248f15
Thomas ZILLIOX <thomas@zilliox.me>
2012-08-22 02:45:06 +0200

    BugFix on the dataTable search

--- Bootstrap/public/dataTables/DT_bootstrap.js
+++ Bootstrap/public/dataTables/DT_bootstrap.js
@@ -96,11 +96,11 @@ $.extend( $.fn.dataTableExt.oPagination, {
 
 /* Table initialisation */
 $(document).ready(function() {
-	$('#example').dataTable( {
+	$('.table-data').dataTable( {
 		"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
 		"sPaginationType": "bootstrap",
 		"oLanguage": {
-			"sLengthMenu": "_MENU_ records per page"
+			"sLengthMenu": "_MENU_ items per page"
 		}
 	} );
 } );
