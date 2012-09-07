4d93169216870c9d4950e5fd4044c4f4f4bb689c
Thomas ZILLIOX <thomas@zilliox.me>
2012-09-07 09:59:10 +0200

    Group the button together

--- Bootstrap/template/module/model/all-action.tpl.php
+++ Bootstrap/template/module/model/all-action.tpl.php
@@ -1,16 +1,20 @@
 <div class="action">
-	<?php
-	if ( $this->from || $this->filter ) {
-	?>
-		<a class="btn" href="<?= $page_url( 'all' ) ?>"><i class="icon-th-list"></i> See all <?= $this->model_type ?></a>
-	<?php
-	} else {
-		foreach ( $this->model_subtypes as $subtype ) {
-		?>
-			<a class="btn" href="<?= $page_url( 'create', $subtype ) ?>"><i class="icon-plus-sign"></i> Create <?= ucfirst( $subtype ) ?></a>
-		<?php
-		}
-	}
-	?>
-	<a class="btn pull-right" href="javascript:window.print( );"><i class="icon-print"></i> Print</a>
+	<div class="btn-toolbar">
+		<div class="btn-group">
+			<?php
+			if ( $this->from || $this->filter ) {
+			?>
+				<a class="btn" href="<?= $page_url( 'all' ) ?>"><i class="icon-th-list"></i> See all <?= $this->model_type ?></a>
+			<?php
+			} else {
+				foreach ( $this->model_subtypes as $subtype ) {
+				?>
+					<a class="btn" href="<?= $page_url( 'create', $subtype ) ?>"><i class="icon-plus-sign"></i> Create <?= ucfirst( $subtype ) ?></a>
+				<?php
+				}
+			}
+			?>
+			<a class="btn" href="javascript:window.print( );"><i class="icon-print"></i> Print</a>
+		</div>
+	</div>
 </div>
