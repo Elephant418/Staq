144d8b12ff27ff842f100a693055683aadbcdb05
Thomas ZILLIOX <thomas@zilliox.me>
2012-09-07 02:09:26 +0200

    Add license

--- Kernel/object/Notification.php
+++ Kernel/object/Notification.php
@@ -1,5 +1,10 @@
 <?php
 
+/* This file is part of the Supersoniq project.
+ * Supersoniq is a free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org/>
+ */
+
 namespace Supersoniq\Kernel\Object;
 
 class Notification implements \Serializable {
