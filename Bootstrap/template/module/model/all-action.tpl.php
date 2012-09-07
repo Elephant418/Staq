9758c19e4c7dbeb6cb20590f6dd82279eac84688
Thomas ZILLIOX <thomas@zilliox.me>
2012-09-07 10:09:41 +0200

    Group the creation button on the model list

--- Bootstrap/template/module/model/all-action.tpl.php
+++ Bootstrap/template/module/model/all-action.tpl.php
@@ -7,10 +7,24 @@
 				<a class="btn" href="<?= $page_url( 'all' ) ?>"><i class="icon-th-list"></i> See all <?= $this->model_type ?></a>
 			<?php
 			} else {
-				foreach ( $this->model_subtypes as $subtype ) {
+				if ( count( $this->model_subtypes ) == 1 ) {
+					$subtype = $this->model_subtypes[ 0 ];
 				?>
 					<a class="btn" href="<?= $page_url( 'create', $subtype ) ?>"><i class="icon-plus-sign"></i> Create <?= ucfirst( $subtype ) ?></a>
 				<?php
+				} else {
+					?>
+					<a class="btn dropdown-toggle" data-toggle="dropdown" href="javascript:;"><i class="icon-plus-sign"></i> Create <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+					<?php
+					foreach ( $this->model_subtypes as $subtype ) {
+					?>
+						<li><a href="<?= $page_url( 'create', $subtype ) ?>"><?= $subtype ?></a></li>
+					<?php
+					}
+					?>
+					</ul>
+					<?php
 				}
 			}
 			?>
