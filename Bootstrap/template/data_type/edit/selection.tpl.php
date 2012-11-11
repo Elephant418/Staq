55e54ed033421dbf814e59b73709e17cda64e30e
Thomas ZILLIOX <thomas@zilliox.me>
2012-11-11 14:28:26 +0100

    BugFix use === to determine the selected value

--- Bootstrap/template/data_type/edit/selection.tpl.php
+++ Bootstrap/template/data_type/edit/selection.tpl.php
@@ -1,10 +1,11 @@
 <select name="model[<?= $this->content->name ?>]">
+	<option value="empty">Votre choix</option>
 <?php
 	foreach ( $this->content->options as $value => $name ) {
 	?>
 		<option 
 			value="<?= $value ?>" 
-			<?= ( $this->content->value() == $value ) ? 'selected="selected"' : '' ?>
+			<?= ( $this->content->value() === $value ) ? 'selected="selected"' : '' ?>
 		> <?= $name ?>
 		</option>
 	<?php
