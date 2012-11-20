cb79f068386e7e22624c538a3264a079018eca41
Thomas ZILLIOX <thomas@zilliox.me>
2012-11-20 18:22:41 +0100

    Add constants and application class loading

--- include.php
+++ include.php
@@ -3,4 +3,8 @@
 /* Todo MIT license
  */
 
+define( 'HTML_EOL', '<br>' . PHP_EOL );
+define( 'STAQ_ROOT_PATH', dirname( __DIR__ ) . '/' );
+
 include( __DIR__ . '/util/functions.php' );
+include( __DIR__ . '/core/Application.php' );
