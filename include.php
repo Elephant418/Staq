f18b853efd5dd031271b8f0334a60b03d8afe490
Thomas ZILLIOX <thomas@zilliox.me>
2012-11-22 17:37:17 +0100

    BugFix: We can include Staq several time

--- include.php
+++ include.php
@@ -3,8 +3,12 @@
 /* Todo MIT license
  */
 
-define( 'HTML_EOL', '<br>' . PHP_EOL );
-define( 'STAQ_ROOT_PATH', dirname( __DIR__ ) . '/' );
+if ( ! defined( 'HTML_EOL' ) ) {
+	define( 'HTML_EOL', '<br>' . PHP_EOL );
+}
+if ( ! defined( 'STAQ_ROOT_PATH' ) ) {
+	define( 'STAQ_ROOT_PATH', dirname( __DIR__ ) . '/' );
+}
 
-include( __DIR__ . '/util/functions.php' );
-include( __DIR__ . '/core/Application.php' );
+include_once( __DIR__ . '/util/functions.php' );
+include_once( __DIR__ . '/core/Application.php' );
