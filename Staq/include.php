15986aa61bf5c1340062a25c4fe61f5c4e9a9126
Thomas ZILLIOX <thomas@zilliox.me>
2013-01-11 03:27:28 +0100

    BugFix: If there are a namespace, nothing can be outside

--- Staq/include.php
+++ Staq/include.php
@@ -4,17 +4,18 @@
 
 namespace Staq {
 	const VERSION = '0.4';
+
+	if ( ! defined( 'HTML_EOL' ) ) {
+		define( 'HTML_EOL', '<br>' . PHP_EOL );
+	}
+	if ( ! defined( 'STAQ_ROOT_PATH' ) ) {
+		define( 'STAQ_ROOT_PATH', dirname( __DIR__ ) . '/' );
+	}
+
+	require_once( __DIR__ . '/util/functions.php' );
+	require_once( __DIR__ . '/util/Ubiq.php' );
+	require_once( __DIR__ . '/core/Server.php' );
+	require_once( __DIR__ . '/core/Application.php' );
+	require_once( __DIR__ . '/core/Autoloader.php' );
 }
 
-if ( ! defined( 'HTML_EOL' ) ) {
-	define( 'HTML_EOL', '<br>' . PHP_EOL );
-}
-if ( ! defined( 'STAQ_ROOT_PATH' ) ) {
-	define( 'STAQ_ROOT_PATH', dirname( __DIR__ ) . '/' );
-}
-
-require_once( __DIR__ . '/util/functions.php' );
-require_once( __DIR__ . '/util/Ubiq.php' );
-require_once( __DIR__ . '/core/Server.php' );
-require_once( __DIR__ . '/core/Application.php' );
-require_once( __DIR__ . '/core/Autoloader.php' );
