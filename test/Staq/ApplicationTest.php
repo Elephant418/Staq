06e6baf99f3bd8c880c796ca89a247252719aba8
Thomas ZILLIOX <thomas@zilliox.me>
2013-01-17 16:05:01 +0100

    Migrating test to phpunit

--- test/Staq/ApplicationTest.php
+++ test/Staq/ApplicationTest.php
@@ -6,9 +6,34 @@ require_once( __DIR__ . '/../../vendor/autoload.php' );
 
 class Application_Test extends \PHPUnit_Framework_TestCase {
 
-	// Without custom application
-	public function test_without_custom_setting( ) {
+
+
+	/*************************************************************************
+	 ATTRIBUTES
+	 *************************************************************************/
+	public $starter_namespaces = [ 'Staq\App\Starter', 'Staq\Core\Router', 'Staq\Core\Ground' ];
+
+
+
+
+	/*************************************************************************
+	  TEST METHODS             
+	 *************************************************************************/
+	public function test_empty_project__extensions( ) {
 		$app = \Staq\Application::create( );
-		$this->assertEquals( [ 'Staq\App\Starter', 'Staq\Core\Router', 'Staq\Core\Ground' ], $app->get_extensions( 'namespace' ) );
+		$this->assertEquals( $this->starter_namespaces, $app->get_extensions( 'namespace' ) );
+	}
+
+	public function test_empty_project__platform( ) {
+		$app = \Staq\Application::create( );
+		$this->assertEquals( 'prod', $app->get_platform( ) );
+	}
+
+	public function test_named_project__extensions( ) {
+		$project_namespace = 'Staq';
+		$app = \Staq\Application::create( $project_namespace );
+		$expected = $this->starter_namespaces;
+		array_unshift( $expected, $project_namespace );
+		$this->assertEquals( $expected, $app->get_extensions( 'namespace' ) );
 	}
 }
