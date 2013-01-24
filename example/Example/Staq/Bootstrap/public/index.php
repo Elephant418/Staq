0394983a2820ba6b754bcd22f7f22df5e62c145e
Thomas ZILLIOX <thomas@zilliox.me>
2013-01-24 15:49:15 +0100

    Add Home page example & default page content

--- example/Example/Staq/Bootstrap/public/index.php
+++ example/Example/Staq/Bootstrap/public/index.php
@@ -6,6 +6,7 @@ require_once( __DIR__ . '/../../../../../vendor/autoload.php' );
 	->set_platform( 'local' )
 	->add_controller( '/', function( ) {
 		$page = new \Stack\View;
+		$page[ 'template' ] = 'home.html';
 		return $page;
 	} )
 	->run( );
