dad93c20056ceba52c410c1a412033ddb00e8e8e
Thomas ZILLIOX <thomas@zilliox.me>
2013-01-24 16:24:37 +0100

    Example of Staq integration without theme

---
+++ example/Example/Staq/Vanilla/public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once( __DIR__ . '/../../../../../vendor/autoload.php' );
+
+\Staq\Application::create( 'Example\\Staq\\Vanilla' )
+	->set_platform( 'local' )
+	->add_controller( '/', function( ) {
+		$page = new \Stack\View;
+		$page[ 'template' ] = 'home.html';
+		return $page;
+	} )
+	->run( );
+
+?>
