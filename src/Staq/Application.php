1cc36c6cc1673e573f7ec9e95fb6f0b468cbed96
Thomas ZILLIOX <thomas@zilliox.me>
2013-01-24 01:18:56 +0100

    Adapt index.php where it is not at the documet root

--- src/Staq/Application.php
+++ src/Staq/Application.php
@@ -12,7 +12,17 @@ abstract class Application {
 	/*************************************************************************
 	  STATIC SHORTHAND METHODS                 
 	 *************************************************************************/
-	public static function create( $path = 'Staq\Core\Ground', $root_uri = '/', $platform = 'prod' ) {
+	public static function create( $path = 'Staq\Core\Ground', $root_uri = NULL, $platform = 'prod' ) {
+		if ( is_null( $root_uri ) ) {
+			if ( isset( $_SERVER[ 'DOCUMENT_ROOT' ] ) && isset( $_SERVER[ 'SCRIPT_FILENAME' ] ) ) {
+				if ( \UString::is_start_with( $_SERVER[ 'SCRIPT_FILENAME' ], $_SERVER[ 'DOCUMENT_ROOT' ] ) ) {
+					$root_uri = \UString::not_start_with( $_SERVER[ 'SCRIPT_FILENAME' ], $_SERVER[ 'DOCUMENT_ROOT' ] );
+				}
+			}
+			if ( is_null( $root_uri ) ) {
+				$root_uri = '/';
+			}
+		}
 		return ( new \Staq\Server )->create_application( $path, $root_uri, $platform );
 	}
 	public static function get_current_application( ) {
