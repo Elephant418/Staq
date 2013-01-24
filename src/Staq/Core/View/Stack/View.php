933d5057a6e1286214bcb051f3a4772b56237c36
Thomas ZILLIOX <thomas@zilliox.me>
2013-01-24 15:29:46 +0100

    BugFix: base_uri must have an ending slash

--- src/Staq/Core/View/Stack/View.php
+++ src/Staq/Core/View/Stack/View.php
@@ -35,7 +35,7 @@ class View extends \Pixel418\Iniliq\ArrayObject {
 		}
 		$this->twig = new \Twig_Environment( $loader, $params );
 		$this[ 'template' ] = 'index.html';
-		$this[ 'base_uri' ] = \Staq\Application::get_base_uri( );
+		$this[ 'base_uri' ] = \UString::end_with( \Staq\Application::get_base_uri( ), '/' );
 	}
 
 
