506aea44bc4c57e984df9fbb000b55039247682d
Thomas ZILLIOX <thomas@zilliox.me>
2013-02-08 04:10:07 +0100

    Let me introduce to you the Date Attribute

--- src/Staq/Core/Data/Stack/Attribute/Date.php
+++ src/Staq/Core/Data/Stack/Attribute/Date.php
@@ -5,7 +5,7 @@
 
 namespace Staq\Core\Data\Stack\Attribute ;
 
-class Date implements \Stack\IAttribute {
+class Date extends Date\__Parent {
 
 
 
@@ -13,18 +13,24 @@ class Date implements \Stack\IAttribute {
 	  PUBLIC USER METHODS             
 	 *************************************************************************/
 	public function get( ) {
-		return DateTime::createFromFormat( 'Y-m-d', $this->seed );
+		return \DateTime::createFromFormat( 'Y-m-d', $this->seed );
 	}
 
 	public function set( $value ) {
+		if ( static::is_valid( $value ) ) {
+            $this->seed = $value;
+		} else if ( is_a( $value, 'DateTime' ) ) {
+			$this->seed = $value->format( 'Y-m-d' );
+		}
+	}
+
+	public static function is_valid( $value ) {
 		if ( is_string( $value ) ) {
 			if ( preg_match( '/^(\d{4})-(\d{2})-(\d{2})$/', $value, $matches ) ) {
 		        if ( checkdate( $matches[ 2 ], $matches[ 3 ], $matches[ 1 ] ) ) {
-		            $this->seed = $value;
+		            return TRUE;
 		        }
 		    }
-		} else if ( is_a( $value, 'DateTime' ) ) {
-			$this->seed = $value->format( 'Y-m-d' );
 		}
 	}
 }
