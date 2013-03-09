a39411a0c677ac503526f5c279a3caeec883067a
Thomas ZILLIOX <thomas@zilliox.me>
2013-03-09 18:40:03 +0100

    Add UINotification util class to help notify your visitors

---
+++ src/Staq/Core/View/Stack/Util/UINotification.php
@@ -0,0 +1,44 @@
+<?php
+
+/* This file is part of the Ubiq project, which is under MIT license */
+
+namespace Staq\Core\Router\Stack\Util;
+
+class UINotification {
+
+
+
+	/*************************************************************************
+	  CONSTANTS				   
+	 *************************************************************************/
+	const INFO = 'info';
+	const SUCCESS = 'success';
+	const ERROR = 'error';
+
+
+
+	/*************************************************************************
+	  CONSTRUCTOR METHODS				   
+	 *************************************************************************/
+	public function __construct( $message, $type = 'info' ) {
+		$info = array( 'message' => $message, 'type' => $type );
+		if ( ! isset( $_SESSION[ 'Staq' ][ 'UINotification' ] ) ) {
+			$_SESSION[ 'Staq' ][ 'UINotification' ] = array( );
+		}
+		$_SESSION[ 'Staq' ][ 'UINotification' ][ ] = $info;
+	}
+
+
+
+	/*************************************************************************
+	  STATIC METHODS				   
+	 *************************************************************************/
+	public static function pull( ) {
+		$messages = array( );
+		if ( isset( $_SESSION[ 'Staq' ][ 'UINotification' ] ) ) {
+			$messages = $_SESSION[ 'Staq' ][ 'UINotification' ];
+			$_SESSION[ 'Staq' ][ 'UINotification' ] = array( );
+		}
+		return $messages;
+	}
+}
