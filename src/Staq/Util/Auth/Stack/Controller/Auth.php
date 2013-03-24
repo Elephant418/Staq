2228f439493ae8ab27f8f8850fd711cd8415c1ea
Thomas ZILLIOX <thomas@zilliox.me>
2013-03-24 15:56:14 +0100

    Use of Eloq to validate & treat auth forms

--- src/Staq/Util/Auth/Stack/Controller/Auth.php
+++ src/Staq/Util/Auth/Stack/Controller/Auth.php
@@ -6,185 +6,202 @@ namespace Staq\Util\Auth\Stack\Controller;
 
 use \Stack\Util\UINotification as Notif;
 
-class Auth extends Auth\__Parent {
+class Auth extends Auth\__Parent
+{
 
 
-
-	/*************************************************************************
-	 CONSTANTS
-	 *************************************************************************/
-	const MSG_INSCRIPTION_VALID = 'You are now connected as %s.';
-	const MSG_INSCRIPTION_KO = 'This username is not free.';
-	const MSG_LOGIN_VALID = 'You are now connected as %s.';
-	const MSG_LOGIN_KO = 'Wrong credentials.';
-	const MSG_LOGOUT_VALID = 'You are now deconnected.';
-
-
-	/*************************************************************************
-	 ATTRIBUTES
-	 *************************************************************************/
-	const CRYPT_SEED = 'dacz:;,aafapojn';
-	static public $currentUser;
-	public static $setting = [
-		'route.inscription.uri'  => '/inscription',
-		'route.login.uri'        => '/login',
-		'route.login.exceptions' => 'NotAllowed',
-		'route.logout.uri'       => '/logout'
-	];
+    /*************************************************************************
+    CONSTANTS
+     *************************************************************************/
+    const MSG_INSCRIPTION_VALID = 'You are now connected as %s.';
+    const MSG_INSCRIPTION_KO = 'This username is not free.';
+    const MSG_LOGIN_VALID = 'You are now connected as %s.';
+    const MSG_LOGIN_KO = 'Wrong credentials.';
+    const MSG_LOGOUT_VALID = 'You are now deconnected.';
 
 
-
-	/*************************************************************************
-	  ACTION METHODS           
-	 *************************************************************************/
-	public function actionInscription( ) {
-		$codes = $this->getCodes( );
-		$form = ( new \Stack\Util\FormHelper )
-			->addField( 'inscription.login', 'login' )
-			->addConstraint( 'login', 'required' )
-			->addField( 'inscription.password', 'password' )
-			->addConstraint( 'password', 'required' )
-			->addField( 'inscription.code', 'code' )
-			->addConstraint( 'code', 'required' )
-			->addConstraint( 'code', function( $field ) use( $codes ){
-				return in_array( $field, $codes );
-			}, 'Bad Code' );
-
-		$values = $form->getValues( );
-		if ( $form->isValid( ) ) {
-			$password = $this->encryptPassword( $values[ 'password' ] );
-			$user = ( new \Stack\Model\User )
-				->set( 'login', $values[ 'login' ] )
-				->set( 'password', $password )
-				->set( 'code', $values[ 'code' ] );
-			try {
-				$saved = FALSE;
-				$saved = $user->save( );
-			} catch ( \PDOException $e ) {
-			}
-			if ( $saved ) {
-				$this->login( $user );
-				Notif::success( sprintf( static::MSG_INSCRIPTION_VALID, $values[ 'login' ] ) );
-				\Staq\Util::httpRedirectUri( $this->getRedirectUri( ) );
-			} else {
-				Notif::error( static::MSG_INSCRIPTION_KO );
-			}
-		}
-		$page = new \Stack\View\Auth\Inscription;
-		$page[ 'form' ] = $values;
-		$page[ 'formErrors' ] = $form->getErrors( );
-		$page[ 'redirect' ] = $this->getRedirectUri( );
-		return $page;
-	}
-
-	public function actionLogin( ) {
-		$form = ( new \Stack\Util\FormHelper )
-			->addField( 'login.login', 'login' )
-			->addConstraint( 'login', 'required' )
-			->addField( 'login.password', 'password' )
-			->addConstraint( 'password', 'required' );
-
-		$values = $form->getValues( );
-		if ( $form->isValid( ) ) {
-			if ( $this->login( $values[ 'login' ], $values[ 'password' ] ) ) {
-				Notif::success( sprintf( static::MSG_LOGIN_VALID, $values[ 'login' ] ) );
-				\Staq\Util::httpRedirectUri( $this->getRedirectUri( ) );
-			} else {
-				Notif::error( static::MSG_LOGIN_KO );
-			}
-		}
-
-		$page = new \Stack\View\Auth\Login;
-		$page[ 'form' ] = $values;
-		$page[ 'formErrors' ] = $form->getErrors( );
-		$page[ 'redirect' ] = $this->getRedirectUri( );
-		return $page;
-	}
-
-	public function actionLogout( ) {
-		$this->logout( );
-		Notif::info( static::MSG_LOGOUT_VALID );
-		\Staq\Util::httpRedirectUri( '/' );
-	}
+    /*************************************************************************
+    ATTRIBUTES
+     *************************************************************************/
+    const CRYPT_SEED = 'dacz:;,aafapojn';
+    static public $currentUser;
+    public static $setting = [
+        'route.inscription.uri' => '/inscription',
+        'route.login.uri' => '/login',
+        'route.login.exceptions' => 'NotAllowed',
+        'route.logout.uri' => '/logout'
+    ];
 
 
+    /*************************************************************************
+    FORMS METHODS
+     *************************************************************************/
+    public function getInscriptionForm()
+    {
+        $codes = $this->getCodes();
+        \Stack\Util\FormFilter::addCustomFilter('existing_code', function ($field) use ($codes) {
+            return in_array($field, $codes);
+        });
+        return (new \Stack\Util\FormHelper)
+            ->addField('login', 'inscription.login')
+            ->addFilter('login', FILTER_SANITIZE_STRING)
+            ->addFilter('login', 'required', 'This field is required')
+            ->addField('password', 'inscription.password')
+            ->addFilter('password', 'required', 'This field is required')
+            ->addField('code', 'inscription.code')
+            ->addFilter('code', 'required', 'This field is required')
+            ->addFilter('code', 'existing_code', 'This is not a valid beta code');
+    }
 
-	/*************************************************************************
-	  PROTECTED METHODS           
-	 *************************************************************************/
-	protected function getRedirectUri( ) {
-		if ( isset( $_POST[ 'redirect' ] ) ) {
-			return $_POST[ 'redirect' ];
-		}
-		if ( isset( $_GET[ 'redirect' ] ) ) {
-			return $_GET[ 'redirect' ];
-		}
-		return \Staq::App()->getCurrentUri( );
-	}
-
-	protected function getCodes( ) {
-		return ( new \Stack\Setting )
-			->parse( $this )
-			->getAsArray( 'code' );
-	}
+    public function getLoginForm()
+    {
+        return (new \Stack\Util\FormHelper)
+            ->addField('login', 'login.login')
+            ->addFilter('login', FILTER_SANITIZE_STRING)
+            ->addFilter('login', 'required', 'This field is required')
+            ->addField('password', 'login.password')
+            ->addFilter('password', 'required', 'This field is required');
+    }
 
 
+    /*************************************************************************
+    ACTION METHODS
+     *************************************************************************/
+    public function actionInscription()
+    {
+        $form = $this->getInscriptionForm();
+        if ($form->isValid()) {
+            $password = $this->encryptPassword($form->getFieldValue('password'));
+            $user = (new \Stack\Model\User)
+                ->set('login', $form->getFieldValue('login'))
+                ->set('password', $password)
+                ->set('code', $form->getFieldValue('code'));
+            try {
+                $saved = FALSE;
+                $saved = $user->save();
+            } catch (\PDOException $e) {
+            }
+            if ($saved) {
+                $this->login($user);
+                Notif::success(sprintf(static::MSG_INSCRIPTION_VALID, $values['login']));
+                \Staq\Util::httpRedirectUri($this->getRedirectUri());
+            } else {
+                Notif::error(static::MSG_INSCRIPTION_KO);
+            }
+        }
+        $page = new \Stack\View\Auth\Inscription;
+        $page['form'] = $form;
+        $page['redirect'] = $this->getRedirectUri();
+        return $page;
+    }
 
-	/*************************************************************************
-	  UTIL METHODS           
-	 *************************************************************************/
-	public function currentUser( ) {
+    public function actionLogin()
+    {
+        $form = $this->getLoginForm();
+        if ($form->isValid()) {
+            if ($this->login($form->getFieldValue('login'), $form->getFieldValue('password'))) {
+                Notif::success(sprintf(static::MSG_LOGIN_VALID, $form->getFieldValue('login')));
+                \Staq\Util::httpRedirectUri($this->getRedirectUri());
+            } else {
+                Notif::error(static::MSG_LOGIN_KO);
+            }
+        }
+        $page = new \Stack\View\Auth\Login;
+        $page['form'] = $form;
+        $page['redirect'] = $this->getRedirectUri();
+        return $page;
+    }
 
-		// Already initialized
-		if ( ! is_null( static::$currentUser ) ) {
-		    return static::$currentUser;
-		}
+    public function actionLogout()
+    {
+        $this->logout();
+        Notif::info(static::MSG_LOGOUT_VALID);
+        \Staq\Util::httpRedirectUri('/');
+    }
 
-		// Find the current user
-		if ( ! isset( $_SESSION[ 'Staq' ][ 'loggedUser' ] ) ) {
-		    $user = FALSE;
-		} else {
-			$user = ( new \Stack\Model\User )->byId( $_SESSION[ 'Staq' ][ 'loggedUser' ] );
-			if ( ! $user->exists( ) ) {
-				$user = FALSE;
-			}
-		}
-		static::$currentUser = $user;
-		return static::$currentUser;
-	}
 
-	public function login( $user, $password = NULL ) {
-		if ( ! is_object( $user ) ) {
-			$user = ( new \Stack\Model\User )->byLogin( $user );
-		}
-		if ( ! $user->exists( ) ) {
-			return FALSE;
-		}
-		if ( ! is_null( $password ) ) {
-			$password = $this->encryptPassword( $password );
-			if ( $user->password !== $password ) {
-				return FALSE;
-			}
-		}
-		$_SESSION[ 'Staq' ][ 'loggedUser' ] = $user->id;
-		static::$currentUser = $user;
-		return TRUE;
-	}
+    /*************************************************************************
+    PROTECTED METHODS
+     *************************************************************************/
+    protected function getRedirectUri()
+    {
+        if (isset($_POST['redirect'])) {
+            return $_POST['redirect'];
+        }
+        if (isset($_GET['redirect'])) {
+            return $_GET['redirect'];
+        }
+        return \Staq::App()->getCurrentUri();
+    }
 
-	public function isLogged( ) {
-		return ( $this->currentUser( ) !== FALSE );
-	}
+    protected function getCodes()
+    {
+        return (new \Stack\Setting)
+            ->parse($this)
+            ->getAsArray('code');
+    }
 
-	public function logout( ) {
-		if ( isset( $_SESSION[ 'Staq' ][ 'loggedUser' ] ) ) {
-			unset( $_SESSION[ 'Staq' ][ 'loggedUser' ] );
-		}
-		static::$currentUser = NULL;
-	}
 
-	public function encryptPassword( $password ) {
-		return sha1( static::CRYPT_SEED . $password );
-	}
+    /*************************************************************************
+    UTIL METHODS
+     *************************************************************************/
+    public function currentUser()
+    {
+
+        // Already initialized
+        if (!is_null(static::$currentUser)) {
+            return static::$currentUser;
+        }
+
+        // Find the current user
+        if (!isset($_SESSION['Staq']['loggedUser'])) {
+            $user = FALSE;
+        } else {
+            $user = (new \Stack\Model\User)->byId($_SESSION['Staq']['loggedUser']);
+            if (!$user->exists()) {
+                $user = FALSE;
+            }
+        }
+        static::$currentUser = $user;
+        return static::$currentUser;
+    }
+
+    public function login($user, $password = NULL)
+    {
+        if (!is_object($user)) {
+            $user = (new \Stack\Model\User)->byLogin($user);
+        }
+        if (!$user->exists()) {
+            return FALSE;
+        }
+        if (!is_null($password)) {
+            $password = $this->encryptPassword($password);
+            if ($user->password !== $password) {
+                return FALSE;
+            }
+        }
+        $_SESSION['Staq']['loggedUser'] = $user->id;
+        static::$currentUser = $user;
+        return TRUE;
+    }
+
+    public function isLogged()
+    {
+        return ($this->currentUser() !== FALSE);
+    }
+
+    public function logout()
+    {
+        if (isset($_SESSION['Staq']['loggedUser'])) {
+            unset($_SESSION['Staq']['loggedUser']);
+        }
+        static::$currentUser = NULL;
+    }
+
+    public function encryptPassword($password)
+    {
+        return sha1(static::CRYPT_SEED . $password);
+    }
 }
 
 ?>
