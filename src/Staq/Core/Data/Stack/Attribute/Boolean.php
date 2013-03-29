46a4d5f84567f1ddd6a07cc5b86ca2bde4fc919b
Thomas ZILLIOX <thomas@zilliox.me>
2013-03-29 01:32:30 +0100

    Add boolean attribute

---
+++ src/Staq/Core/Data/Stack/Attribute/Boolean.php
@@ -0,0 +1,31 @@
+<?php
+
+/* This file is part of the Staq project, which is under MIT license */
+
+
+namespace Staq\Core\Data\Stack\Attribute ;
+
+class Boolean extends Boolean\__Parent {
+
+
+
+	/*************************************************************************
+	  PUBLIC USER METHODS             
+	 *************************************************************************/
+	public function get( ) {
+		return ($this->seed!==0);
+	}
+
+	public function set( $value ) {
+		$this->seed = $value?1:0;
+	}
+
+
+
+    /*************************************************************************
+    PUBLIC DATABASE METHODS
+     *************************************************************************/
+    public function setSeed( $seed ) {
+        $this->seed = (int)$seed;
+    }
+}
