6fcfd305df024ae5085d4bb48ef293086bf06bb3
Thomas ZILLIOX <thomas@zilliox.me>
2013-03-30 19:09:40 +0100

    Add static page controller

---
+++ src/Staq/Core/Router/Stack/Controller/Page.php
@@ -0,0 +1,35 @@
+<?php
+
+/* This file is part of the Staq project, which is under MIT license */
+
+
+namespace Staq\Core\Router\Stack\Controller;
+
+class Page extends Page\__Parent
+{
+
+
+    /*************************************************************************
+    ATTRIBUTES
+     *************************************************************************/
+    public static $setting = [
+        'route.view.uri' => '/(:page)'
+    ];
+
+
+    /*************************************************************************
+    ACTION METHODS
+     *************************************************************************/
+    public function actionView($page='index')
+    {
+        print_r( \Staq::App()->getExtensions());
+        $path = \Staq::App()->getFilePath('page/' . $page . '.html');
+        if ( $path !== FALSE ) {
+            $page = new \Stack\View\Page;
+            $page['content'] = file_get_contents($path);
+            return $page;
+        }
+    }
+}
+
+?>
