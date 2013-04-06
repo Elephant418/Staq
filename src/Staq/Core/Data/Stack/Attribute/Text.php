5eca64e5020abb9b68ea63991e1ba538c1bbc72a
Thomas ZILLIOX <thomas@zilliox.me>
2013-04-06 20:02:20 +0200

    Add template & tempalting methods for Text attribute

--- src/Staq/Core/Data/Stack/Attribute/Text.php
+++ src/Staq/Core/Data/Stack/Attribute/Text.php
@@ -17,6 +17,22 @@ class Text extends Text\__Parent
         return str_replace( PHP_EOL, HTML_EOL, $this->seed );
     }
 
+    public function getFirstParagraph()
+    {
+        $paragraphs = explode( PHP_EOL, $this->seed, 1);
+        return $paragraphs[0];
+    }
+
+    public function getBeginning( $limit=300 )
+    {
+        $beginning = $this->getFirstParagraph();
+        if ( strlen($beginning) <= $limit ) {
+            return $beginning;
+        }
+        $beginning = substr($beginning, 0, $limit);
+        return implode( '.', explode( '.', $beginning, -1) ) . '...';
+    }
+
     public function set($value)
     {
         $value = str_replace( HTML_EOL, PHP_EOL, $value );
