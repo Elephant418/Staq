0ba19e0e355cfd922406f022790ee9d2ffc82eb5
Thomas ZILLIOX <thomas@zilliox.me>
2013-06-12 00:07:00 +0200

    Add a page title attribute with the name as default value

---
+++ src/Staq/Core/Data/Stack/Model/Page.php
@@ -0,0 +1,15 @@
+<?php
+
+/* This file is part of the Staq project, which is under MIT license */
+
+
+namespace Staq\Core\Data\Stack\Model;
+
+class Page extends Page\__Parent
+{
+    public function initialize() {
+        if (empty($this->title)) {
+            $this->set('title', $this->name);
+        }
+    }
+}
